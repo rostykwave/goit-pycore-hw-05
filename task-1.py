d10e0a96236483c643d316a254cc358168b45aba
Rostyslav <rostykwave@gmail.com>
2025-03-13 18:53:44 +0200

    feat(tak-1)

---
+++ task-1.py
@@ -0,0 +1,25 @@
+from typing import Dict, Callable
+
+def caching_fibonacci() -> Callable[[int], int]:
+    cache: Dict[int, int] = {}
+    def fibonacci(n: int) -> int:
+        if n <= 0:
+            return 0
+        if n == 1:
+            return 1
+        if n in cache:
+            return cache[n]
+        
+        cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+        return cache[n]
+
+    return fibonacci
+
+# Приклад використання
+if __name__ == "__main__":
+    # Отримуємо функцію fibonacci
+    fib = caching_fibonacci()
+
+    # Використовуємо функцію fibonacci для обчислення чисел Фібоначчі
+    print(fib(10))  # Виведе 55
+    print(fib(15))  # Виведе 610
